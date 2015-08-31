d240539d402e3b1e8af8dc9ee310bf620aae1092
John Bellone <jbellone@bloomberg.net>
2015-08-31 10:27:53 -0400

    Implements the Clojure service configuration resource.
    This resource uses the [edn configuration
    format](https://github.com/edn-format/edn).

--- libraries/clojure_config.rb
+++ libraries/clojure_config.rb
@@ -8,9 +8,39 @@ require 'poise'
 
 module ClojureService
   module Resource
+    # A resource to manage Clojure application configurations.
+    # These adheres to the edn configuration format.
+    # @see https://github.com/edn-format/edn
+    # @since 1.0.0
+    # @example
     class ClojureConfig < Chef::Resource
       include Poise(fused: true)
       provides(:clojure_config)
+
+      attribute(:path, kind_of: String, name_attribute: true)
+      attribute(:owner, kind_of: String, default: 'clojure')
+      attribute(:group, kind_of: String, default: 'clojure')
+      attribute(:mode, kind_of: String, default: '0640')
+
+      attribute(:options, option_collector: true)
+
+      action(:create) do
+        include_recipe 'clojure-service::default'
+        notifying_block do
+          file new_resource.path do
+            content new_resource.options.to_edn
+            owner new_resource.owner
+            group new_resource.group
+            mode new_resource.mode
+          end
+        end
+      end
+
+      action(:delete) do
+        file new_resource.path do
+          action :delete
+        end
+      end
     end
   end
 end
